2e620cdfcda2b0481f561625fc382b009db013bf
Lyn Nagara <lyn.nagara@gmail.com>
2017-04-11 22:21:29 +0200

    Use Contentful data

--- lambda/slack.js
+++ lambda/slack.js
@@ -4,13 +4,15 @@ const WebClient = require('@slack/client').WebClient
 const token = process.env.SLACK_API_TOKEN
 const web = new WebClient(token)
 const contentful = require('./contentful')
-const MESSAGE = 'here is a fresh chemex'
-
 const contentfulConfig = contentful.getConfig()
 
 function sendSlackMessage(event) {
   contentfulConfig.then(function (config) {
-    web.chat.postMessage('slack-tests', MESSAGE, function(err, res) {
+    const channel = config.configuration[event.serialNumber].channel
+    const messageIdx = Math.floor(Math.random() * config.message.length)
+    const message = config.message[messageIdx].message
+
+    web.chat.postMessage(channel, message, function(err, res) {
       if (err) {
         console.log('Error:', err)
       } else {
