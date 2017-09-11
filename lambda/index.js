9dc6a261c1f0412067bc606a2269f49a39f4ba76
Lyn Nagara <lyn.nagara@gmail.com>
2017-09-11 18:51:21 +0200

    refactor

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,14 @@
 'use strict';
 
+const contentful = require('./contentful')
 const slack = require('./slack')
 
-exports.handler = (event, context, callback) => {
-  slack(event)
+exports.handler = (event) => {
+  contentful.getAll().then(function (data) {
+    const {channel} = data.configuration[event.serialNumber]
+    const randomMessageIdx = Math.floor(Math.random() * data.message.length)
+    const {message} = data.message[randomMessageIdx]
+
+    slack.send(channel, message)
+  })
 }
--- lambda/slack.js
+++ lambda/slack.js
@@ -1,25 +1,18 @@
 'use strict';
 
-const WebClient = require('@slack/client').WebClient
 const token = process.env.SLACK_API_TOKEN
 const web = new WebClient(token)
-const contentful = require('./contentful')
-let contentfulConfig = contentful.getConfig()
 
-function sendSlackMessage(event) {
-  contentfulConfig.then(function (config) {
-    const channel = config.configuration[event.serialNumber].channel
-    const messageIdx = Math.floor(Math.random() * config.message.length)
-    const message = config.message[messageIdx].message
-
-    web.chat.postMessage(channel, message, function(err, res) {
-      if (err) {
-        console.log('Error:', err)
-      } else {
-        console.log('Message sent: ', res)
-      }
-    })
+function send(channel, message) {
+  web.chat.postMessage(channel, message, function(err, res) {
+    if (err) {
+      console.log('Error:', err)
+    } else {
+      console.log('Message sent: ', res)
+    }
   })
 }
 
-module.exports = sendSlackMessage
+module.exports = {
+  send
+}
