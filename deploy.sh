1640908b9053cfdf3404c8c8287bc9471292471e
Joshua Smock <github@joshuasmock.com>
2018-01-16 22:10:37 +0100

    Make deploy script more robust

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
-rm index.zip
-cd lambda
-zip -r -X ../index.zip *
-cd ..
-aws lambda update-function-code --function-name chemex-first-floor --zip-file fileb://index.zip
+
+usage() {
+    echo "Usage: deploy.sh <lambda-function-name>"
+}
+
+if [ -z "$1" ]; then
+    echo "Provide a function name."
+    usage
+    exit 1
+fi
+
+# Test AWS existence
+which aws 1>/dev/null 2>&1
+
+if [ "$?" -ne "0" ]; then
+    echo "Please install the AWS cli tools."
+    exit 2
+fi
+
+# Remove the zip file, if it exists
+rm super-chemex-bot.zip 2>/dev/null
+
+echo "Zipping the lambda/ directory..."
+zip -r -X super-chemex-bot.zip lambda/*
+
+echo "Deploying to AWS..."
+aws lambda update-function-code --function-name $1 --zip-file fileb://super-chemex-bot.zip
+
+if [ "$?" -eq "255" ]; then
+    echo "super-chemex-bot couldn't deploy to AWS properly. Check the function name and try again."
+else
+    echo "super-chemex-bot deployed! Test with 'test.sh'."
+fi
